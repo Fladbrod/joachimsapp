89da1cbb8fa482f9b4257813d1e9d1d00f89ca6a
Fladbrod <73465595+Fladbrod@users.noreply.github.com>
2021-09-26 19:29:56 +0200

    make it better

---
+++ src/main/java/heroku/joachimsapp/JoachimsappApplication.java
@@ -0,0 +1,13 @@
+package heroku.joachimsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoachimsappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoachimsappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/heroku/joachimsapp/JoachimsappApplicationTests.java
@@ -0,0 +1,13 @@
+package heroku.joachimsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoachimsappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
